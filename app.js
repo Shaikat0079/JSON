d6702ba7f0dfc2e8c63a97018510e3a982c997a8
Shaikat0079 <baruashaikat00790@gmail.com>
2025-03-11 05:15:39 +0930

    Fetching

---
+++ app.js
@@ -0,0 +1,15 @@
+const person = {
+    pName: "Shaikat",
+    age: 27,
+    friends: ['sayed','arafat','Tauhid'],
+    visaStatus: true
+}
+
+const newPerson = JSON.stringify(person)
+
+console.log(newPerson)
+console.log(typeof(newPerson))
+console.log(typeof(person))
+
+const newPersonSimplify = JSON.parse(newPerson)
+console.log(newPersonSimplify)
